8c791fe08eaf46c64e0b6ec5430709de351f49b8
SiyiZhao <siyizhao17@gmail.com>
2025-06-11 04:36:05 -0700

    2PCF measure

---
+++ clustering/README.md
@@ -0,0 +1,9 @@
+# Measure the clustering 
+
+We use the script `xirunpc.py` used in DESI to measure the clustering of the galaxy catalogs.
+
+Pre-requisites:
+- [LSS](https://github.com/desihub/LSS): This code only works on NERSC, in the DESI environment. 
+
+We try the following measurements:
+- log-bin, s=0.01-30 Mpc/h (notice clustering measurement at s>40Mpc/h is forbidden before the key project is finished.)
---
+++ clustering/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load python
+source /global/cfs/cdirs/desi/software/desi_environment.sh main
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+export PYTHONPATH=$PYTHONPATH:$HOME/lib/LSS/py
+
+weight_type=pip_angular_bitwise
+version=v1.1/PIP
+verspec=loa-v1
+corr=smu
+outdir=/global/homes/s/siyizhao/data/Y3/$verspec
+
+srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g /global/homes/s/siyizhao/projects/fihobi/clustering/xirunpc.py --tracer LRG --survey DA2 --verspec $verspec --nthreads 256 --version $version --region NGC SGC --corr_type $corr --njack 128 --zlim 0.6 0.8 --weight_type $weight_type --bin_type log --nreal=128 --outdir $outdir > $outdir/stdout.log 2> $outdir/stderr.log ; exit
