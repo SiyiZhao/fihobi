042c678b3e8e4edab882c48d009967b69f25a236
SiyiZhao <siyizhao17@gmail.com>
2025-07-21 22:16:36 -0700

    measure Y3 wp

--- clustering/pcforfit.py
+++ clustering/pcforfit.py
@@ -15,22 +15,33 @@ from pycorr import TwoPointCorrelationFunction
 DEFAULTS = {}
 DEFAULTS['dir'] = '/global/homes/s/siyizhao/data/Y3/loa-v1'
 DEFAULTS['fix'] = 'LRG_GCcomb_0.6_0.8_pip_angular_bitwise_log_njack128_nran4_split20'
+DEFAULTS['mode'] = 'smu'  # Default mode is 'smu' for multipoles
 
-def main(dir, fix, check):
+def check_and_report(arr1, arr2, name1, name2):
+    if np.allclose(arr1, arr2):
+        print(f"{name1} and {name2} match.")
+    else:
+        # Ignore positions where either array has nan
+        mask = ~np.isnan(arr1) & ~np.isnan(arr2)
+        if np.allclose(arr1[mask], arr2[mask]):
+            print(f"{name1} and {name2} match (ignoring NaNs).")
+        else:
+            print(f"{name1} and {name2} do NOT match (ignoring NaNs).")
+            diff = np.abs((arr1[mask] - arr2[mask]) / arr2[mask])
+            print(f"Max difference fraction: {diff.max()} at index {diff.argmax()}")
+
+def pcforfit_poles(dir, fix, check=False):
     pycorr_out_path = dir + f'/smu/allcounts_{fix}.npy'
-
-    ## define s edges and log midpoints
-    # sedges = np.geomspace(0.01, 30., 41)
-    # s_log_mid = 10**((np.log10(sedges[:-1]) + np.log10(sedges[1:])) / 2)
 
     ## load the TwoPointCorrelationFunction result
     result = TwoPointCorrelationFunction.load(pycorr_out_path)
+
     sep, corr, cov = result.get_corr(mode='poles', return_sep=True, return_cov=True)
     num = sep.shape[0]
     xi0 = corr[0, :]
     xi2 = corr[1, :]
-
     cov00 = cov[:num, :num]
+    cov22 = cov[num:2*num, num:2*num]
     
     ## cut the nan part
     mask = ~np.isnan(xi0)
@@ -38,36 +49,82 @@ def main(dir, fix, check):
     xi0_sel = xi0[mask]
     xi2_sel = xi2[mask]
     cov00_sel = cov00[mask][:, mask]
+    cov22_sel = cov22[mask][:, mask]  # Assuming cov22 is the same as cov00 for xi2
     if np.isnan(cov00_sel).any():
         print("Warning: cov00_sel contains NaN values.")
+    if np.isnan(cov22_sel).any():
+        print("Warning: cov22_sel contains NaN values.")
 
     ## save 
-    np.savetxt(dir + f'/hodfit/xipoles_{fix}.txt', np.column_stack((sep_sel, xi0_sel, xi2_sel)), header='sep xi0 xi2')
-    np.save(dir + f'/hodfit/cov_xi0_{fix}.npy', cov00_sel)
+    np.savetxt(dir + f'/hodfit/xipoles_{fix}.dat', np.column_stack((sep_sel, xi0_sel, xi2_sel)), header='sep xi0 xi2')
+    np.save(dir + f'/hodfit/cov_xi0_{fix}.dat', cov00_sel)
+    np.save(dir + f'/hodfit/cov_xi2_{fix}.dat', cov22_sel)
 
     ## check the xi0 and std with .txt file
     if check==True:
         xi0_std = np.sqrt(np.diag(cov00))
-        ref = np.loadtxt(dir + '/smu/xipoles_LRG_GCcomb_0.6_0.8_pip_angular_bitwise_log1_njack128_nran4_split20.txt')
+        xi2_std = np.sqrt(np.diag(cov22))
+        if 'log' in fix:
+            fix1 = fix.replace('log', 'log1')
+        elif 'lin' in fix:
+            fix1 = fix.replace('lin', 'lin1')
+        else:
+            fix1 = fix
+        ref = np.loadtxt(dir + f'/smu/xipoles_{fix1}.txt')
         savg = ref[:, 1]
         corr0 = ref[:, 2]
+        corr2 = ref[:, 3]
         std0 = ref[:, 5]
-        def check_and_report(arr1, arr2, name1, name2):
-            if np.allclose(arr1, arr2):
-                print(f"{name1} and {name2} match.")
-            else:
-                # Ignore positions where either array has nan
-                mask = ~np.isnan(arr1) & ~np.isnan(arr2)
-                if np.allclose(arr1[mask], arr2[mask]):
-                    print(f"{name1} and {name2} match (ignoring NaNs).")
-                else:
-                    print(f"{name1} and {name2} do NOT match (ignoring NaNs).")
-                    diff = np.abs(arr1[mask] - arr2[mask])
-                    print(f"Max difference: {diff.max()} at index {diff.argmax()}")
+        std2 = ref[:, 6]
 
         check_and_report(savg, sep, "savg", "sep")
         check_and_report(corr0, xi0, "corr0", "xi0")
         check_and_report(std0, xi0_std, "std0", "xi0_std")
+        check_and_report(corr2, xi2, "corr2", "xi2")
+        check_and_report(std2, xi2_std, "std2", "cov22_std")
+
+def pcforfit_wp(dir, fix, check=False):
+    pycorr_out_path = dir + f'/rppi/allcounts_{fix}.npy'
+    result = TwoPointCorrelationFunction.load(pycorr_out_path)
+    sep, corr, cov = result.get_corr(mode='wp', return_sep=True, return_cov=True)
+    
+    mask = (sep >= 0.1) & (sep <= 30.0)
+    rp = sep[mask]
+    wp = corr[mask]
+    cov_wp = cov[mask][:, mask]
+    ## save
+    np.savetxt(dir + f'/hodfit/wp_{fix}.dat', np.column_stack((rp, wp)), header='rp wp')
+    np.savetxt(dir + f'/hodfit/cov_wp_{fix}.dat', cov_wp)
+    
+    if check:
+        if 'log' in fix:
+            fix1 = fix.replace('log', 'log1')
+        elif 'lin' in fix:
+            fix1 = fix.replace('lin', 'lin1')
+        else:
+            fix1 = fix
+        ref = np.loadtxt(dir + f'/rppi/wp_{fix1}.txt')
+        rpavg = ref[:, 1]
+        corr_wp = ref[:, 2]
+        std_wp = ref[:, 3]
+
+        check_and_report(rpavg, sep, "rpavg", "sep")
+        check_and_report(corr_wp, corr, "corr_wp", "corr")
+        cov_std = np.sqrt(np.diag(cov))
+        check_and_report(std_wp, cov_std, "std_wp", "cov_std")
+
+def main(dir, fix, mode, check):
+    
+    ## define s edges and log midpoints
+    # sedges = np.geomspace(0.01, 30., 41)
+    # s_log_mid = 10**((np.log10(sedges[:-1]) + np.log10(sedges[1:])) / 2)
+
+    if mode=='smu':
+        pcforfit_poles(dir, fix, check)
+    elif mode=='wp':
+        pcforfit_wp(dir, fix, check)
+    else:
+        raise ValueError("Unsupported mode. Use 'smu' or 'wp'.")
 
 
 class ArgParseFormatter(
@@ -85,6 +142,10 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--fix', help='Fix string for the output files', default=DEFAULTS['fix']
+    )
+    parser.add_argument(
+        '--mode', choices=['smu', 'wp'], default=DEFAULTS['mode'],
+        help='Mode of the two-point correlation function: smu for multipoles, wp for projected correlation function.'
     )
     parser.add_argument(
         '--check', action='store_true',
---
+++ clustering/run_wp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+module load python
+source /global/cfs/cdirs/desi/software/desi_environment.sh main
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+export PYTHONPATH=$PYTHONPATH:$HOME/lib/LSS/py
+
+weight_type=pip_angular_bitwise
+version=v1.1/PIP
+verspec=loa-v1
+corr=rppi
+outdir=/global/homes/s/siyizhao/data/Y3/$verspec
+# outdir=/pscratch/sd/s/siyizhao/fihobi/Y3_log/$verspec
+mkdir -p $outdir
+
+srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g /global/homes/s/siyizhao/projects/fihobi/clustering/xirunpc.py --tracer LRG --survey DA2 --verspec $verspec --nthreads 256 --version $version --region NGC SGC --corr_type $corr --njack 128 --zlim 0.6 0.8 --weight_type $weight_type --bin_type log --nreal=128 --outdir $outdir > $outdir/rppi_stdout.log 2> $outdir/rppi_stderr.log
+
+mkdir -p $outdir/hodfit
+srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python pcforfit.py --dir $outdir --fix LRG_GCcomb_0.6_0.8_pip_angular_bitwise_log_njack128_nran4_split20 --mode wp --check > $outdir/wpforfit_stdout.log 2> $outdir/wpforfit_stderr.log
+
+exit
