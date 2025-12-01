e2761f69ce1399bb0faeb20eb3b609389c20db29
SiyiZhao <siyizhao17@gmail.com>
2025-11-30 20:06:48 -0800

    v2 running for LRG-z1 with assembly and QSO-z6 all base

--- hod-variation/launchers/QSO-fnl100_z6_base-B.sh
+++ hod-variation/launchers/QSO-fnl100_z6_base-B.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=QSO-fnl100_z6_base
+#SBATCH --job-name=QSO-fnl100_z6_base-B
 #SBATCH --output=/global/homes/s/siyizhao/projects/fihobi/hod-variation/logs/%x_%j.log
 #SBATCH --error=/global/homes/s/siyizhao/projects/fihobi/hod-variation/logs/%x_%j.err
 #SBATCH --qos=regular
@@ -15,9 +15,9 @@ source /global/common/software/desi/desi_environment.sh
 export PYTHONPATH=$PYTHONPATH:$HOME/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib/MultiNest/lib
 export OMP_NUM_THREADS=64
-outdir=/pscratch/sd/s/siyizhao/desi-dr2-hod/QSO-fnl100/z6_base/
+outdir=/pscratch/sd/s/siyizhao/desi-dr2-hod/QSO-fnl100/z6_base-B/
 mkdir -p $outdir
-config=configs/QSO-fnl100/z6_base.yaml
+config=configs/QSO-fnl100/z6_base-B.yaml
 cd /global/homes/s/siyizhao/projects/fihobi/hod-variation
 
 # srun -n 1 -c 64 --cpu-bind=cores python -m abacusnbody.hod.prepare_sim_profiles --path2config $config
