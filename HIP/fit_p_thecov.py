690e89c8f6fc2849b877f7f47b8a7fa8c632a1bc
SiyiZhao <siyizhao17@gmail.com>
2025-12-15 22:50:27 -0800

    move core part to src/desilike_helper

--- HIP/fit_p_thecov.py
+++ HIP/fit_p_thecov.py
@@ -1,10 +1,6 @@
 import numpy as np
 import yaml
-from desilike.theories.galaxy_clustering import FixedPowerSpectrumTemplate, PNGTracerPowerSpectrumMultipoles
-from desilike.observables.galaxy_clustering import TracerPowerSpectrumMultipolesObservable
-from desilike.likelihoods import ObservablesGaussianLikelihood
 
-from desilike.profilers import MinuitProfiler
 from desilike.samplers import ZeusSampler
 from desilike.samples import plotting
 
@@ -30,6 +26,7 @@ import sys, os
 sys.path.insert(0, os.path.expanduser('../src'))
 from io_def import read_catalog
 from pypower_helpers import run_pypower_redshift
+from desilike_helper import prepare_theory_fix_fNL, bestfit_p_inference
 
 # kmax = 0.10053096491487339
 # # kedges = np.linspace(0, kmax, 32)
@@ -139,41 +136,15 @@ cov = read_cov(path2dir + f'{prefix}_thecov_sn_gaussian_t0.npy')
 
 ## PNG likelihood
 print('Setting up likelihood ...')
-template = FixedPowerSpectrumTemplate(z=z, fiducial=cosmology)
-# fnl_loc is degenerate with PNG bias bphi. Parameterization is controlled by "mode".
-# - "b-p": bphi = 2 * 1.686 * (b1 - p), p as a parameter
-# - "bphi": bphi as a parameter
-# - "bfnl_loc": bfnl_loc = bphi * fnl_loc as a parameter'
-# Here we choose b-p parameterization
-theory = PNGTracerPowerSpectrumMultipoles(template=template, mode=mode)
-## fixed fNL
-theory.init.params['fnl_loc'].update(value=fnl, fixed=True) 
-## other parameters may need larger prior ranges
 priors = config.get('prior', {})
-for key in priors:
-    theory.init.params[key].update(fixed=False, prior=priors[key])
+theory = prepare_theory_fix_fNL(z=z, mode=mode, fnl=fnl, priors=priors)
 ## status of all parameters
 for key in theory.params:
     print(key, theory.params[key].value, theory.params[key].fixed, theory.params[key].derived, theory.params[key].prior, theory.params[key].ref)
-observable = TracerPowerSpectrumMultipolesObservable(data=data['P_0'], covariance=cov, ells=ells, k=data['k'],
-        klim={0: klim0},
-        # klim={0: [0.005, 0.2, 0.005], 2: [0.005, 0.2, 0.005]}, # fit monopole and quadrupole, between 0.005 and 0.2 h/Mpc
-        theory=theory)
 
-likelihood = ObservablesGaussianLikelihood(observables=[observable])
-
-likelihood()  # just to initialize
-
+bestfit_dict, likelihood, observable = bestfit_p_inference(theory=theory, data=data['P_0'], cov=cov, ells=ells, k=data['k'], klim={0: klim0})
 
 ## sampling
-print('Minuit Profiler...')
-# Seed used to decide on starting point
-profiler = MinuitProfiler(likelihood, seed=42)
-# Find best fit, starting from 5 different starting points
-# NOTE: With MPI, these runs are performed in parallel
-profiles = profiler.maximize(niterations=5)
-print(profiles.to_stats(tablefmt='pretty'))
-
 print('Zeus Sampler...')
 sampler = ZeusSampler(likelihood, seed=42)
 sampler.run(check={'max_eigen_gr': 0.04})
@@ -181,13 +152,6 @@ chain = sampler.chains[0].remove_burnin(0.5)
 chain.write_getdist(chain_fn)
 print(chain.to_stats(tablefmt='pretty'))
 
-
-## bestfit
-bestfit_dict = {}
-for key in theory.all_params:
-    bestfit_value = profiles.bestfit.choice(input=True)[str(key)].value
-    theory.init.params[key].update(value=bestfit_value)
-    bestfit_dict[str(key)] = bestfit_value
 
 ## plot triangle
 if mode == 'b-p':
---
+++ src/desilike_helper.py
@@ -0,0 +1,71 @@
+import numpy as np
+from desilike.theories.galaxy_clustering import FixedPowerSpectrumTemplate, PNGTracerPowerSpectrumMultipoles
+from desilike.observables.galaxy_clustering import TracerPowerSpectrumMultipolesObservable
+from desilike.likelihoods import ObservablesGaussianLikelihood
+
+from desilike.profilers import MinuitProfiler
+
+
+def prepare_theory_fix_fNL(
+    z: float,
+    cosmology: str='DESI',
+    mode: str='b-p',
+    fnl: float=0,
+    priors: dict[str, list]=dict(),
+):
+    
+    template = FixedPowerSpectrumTemplate(z=z, fiducial=cosmology)
+    # fnl_loc is degenerate with PNG bias bphi. Parameterization is controlled by "mode".
+    # - "b-p": bphi = 2 * 1.686 * (b1 - p), p as a parameter
+    # - "bphi": bphi as a parameter
+    # - "bfnl_loc": bfnl_loc = bphi * fnl_loc as a parameter'
+    theory = PNGTracerPowerSpectrumMultipoles(template=template, mode=mode)
+    
+    ## fixed fNL
+    theory.init.params['fnl_loc'].update(value=fnl, fixed=True) 
+
+    ## other parameters may need larger prior ranges
+    # priors = config.get('prior', {})
+    for key in priors:
+        theory.init.params[key].update(fixed=False, prior=priors[key])
+    return theory
+
+def bestfit_p_inference(
+    theory: PNGTracerPowerSpectrumMultipoles,
+    data,
+    cov,
+    ells: list[int] =[0],
+    k: np.ndarray | list[np.ndarray] | None = None,
+    klim: dict = {0: [0.005, 0.2, 0.005]},
+) -> dict:
+    """
+    Use Minuit Profiler to find bestfit parameters.
+    Data: monopole power spectrum from mocks.
+    Covariance: provided covariance matrix.
+    k: if one only provided simple arrays for data and covariance, one can provide the corresponding (list of) k wavenumbers as a (list of) array k.
+    """
+    observable = TracerPowerSpectrumMultipolesObservable(data=data, covariance=cov, ells=ells, k=k, klim=klim, theory=theory)
+    
+    likelihood = ObservablesGaussianLikelihood(observables=[observable])
+
+    likelihood()  # just to initialize
+
+    ## sampling
+    print('Minuit Profiler...')
+    # Seed used to decide on starting point
+    profiler = MinuitProfiler(likelihood, seed=42)
+    # Find best fit, starting from 5 different starting points
+    # NOTE: With MPI, these runs are performed in parallel
+    profiles = profiler.maximize(niterations=5)
+    print(profiles.to_stats(tablefmt='pretty'))
+
+
+    ## bestfit
+    # best_p = profiles.bestfit.choice(input=True)['p'].value
+    bestfit_dict = {}
+    for key in theory.all_params:
+        bestfit_value = profiles.bestfit.choice(input=True)[str(key)].value
+        # theory.init.params[key].update(value=bestfit_value)
+        bestfit_dict[str(key)] = bestfit_value
+    # best_p = bestfit_dict['p']
+    return bestfit_dict, likelihood, observable
