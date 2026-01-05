4517f9793539a92a9d65aadbf67709aee67e7bdd
SiyiZhao <siyizhao17@gmail.com>
2026-01-05 05:14:33 -0800

    test on ph001 box

--- fit_fNL_HIP/fit_fNL_HIP.sh
+++ fit_fNL_HIP/fit_fNL_HIP.sh
@@ -5,7 +5,7 @@ cd ${THIS_REPO}
 
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 
-odir=fit_fNL_HIP/works/QSO_box_z3_base-A_MAP_thecov
+odir=fit_fNL_HIP/works/QSO_box_z3_base-A_ph001_thecov
 mkdir -p ${odir}
 config=${odir}/config.yaml
 
